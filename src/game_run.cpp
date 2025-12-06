df3e652990b1a32e303866caa10196fc2fb7db18
Francisco <franjonas98@hotmail.com>
2025-12-05 21:09:36 -0300

    Add principal functions to main loop

--- src/game_run.cpp
+++ src/game_run.cpp
@@ -8,8 +8,42 @@
 
 namespace finalX
 {
+	GameStats gameStatus;
+
+	static void Init();
+	static void Input();
+	static void Update();
+	static void Draw();
+
 	void game_run()
 	{
+		Init();
+		
+		while (!WindowShouldClose())
+		{
+			Input();
+			Update();
+			Draw();
+		}
 
+		CloseWindow();
+	}
+
+	static void Init()
+	{
+		const int screenWidth = 1024;
+		const int screenHeight = 768;
+
+		InitWindow(screenWidth, screenHeight, "finalX");
+		InitAudioDevice();
+	}
+	void Input()
+	{
+	}
+	void Update()
+	{
+	}
+	void Draw()
+	{
 	}
 }
