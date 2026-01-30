e0bc689ae23ecb044b327dc3f5ed0b2851be18c6
Francisco <franjonas98@hotmail.com>
2026-01-29 21:16:53 -0300

    Add input and update to player for movemment

--- src/entities/player.cpp
+++ src/entities/player.cpp
@@ -5,6 +5,8 @@ namespace tron_seekers
 {
 	GameStats aux_game_stats;
 
+	static void ChangePLayerDir(Player& player);
+
 	void InitPlayer(Player& player)
 	{
 		float player_width = 20.0f;
@@ -17,6 +19,42 @@ namespace tron_seekers
 		player.player_hit_box.y = player.player_pos.y;
 		player.player_hit_box.width = player_width;
 		player.player_hit_box.height = player_height;
+
+		player.player_speed = 450.0f;
+	}
+
+	void InputPlayer(Player& player)
+	{
+		ChangePLayerDir(player);
+	}
+
+	void UpdatePlayer(Player& player)
+	{
+		switch (player.player_direction)
+		{
+		case EntitieMovemment::UP:
+			player.player_hit_box.y -= player.player_speed * GetFrameTime();
+			break;
+
+		case EntitieMovemment::RIGTH:
+			player.player_hit_box.x += player.player_speed * GetFrameTime();
+			break;
+
+		case EntitieMovemment::DOWN:
+			player.player_hit_box.y += player.player_speed * GetFrameTime();
+			break;
+
+		case EntitieMovemment::LEFT:
+			player.player_hit_box.x -= player.player_speed * GetFrameTime();
+			break;
+
+		case EntitieMovemment::NONE:
+			player.player_hit_box = player.player_hit_box;
+			break;
+
+		default:
+			break;
+		}
 	}
 
 	void DrawPlayer(Player player)
@@ -25,5 +63,35 @@ namespace tron_seekers
 		Color aux_player_color = RED;
 
 		DrawRectangleLinesEx(player.player_hit_box, aux_rec_lines, aux_player_color);
+	}
+
+	static void ChangePLayerDir(Player& player)
+	{
+		struct KeyDir
+		{
+			KeyboardKey key;
+			EntitieMovemment dir;
+		};
+
+		KeyDir inputs[] =
+		{
+			{KEY_W, EntitieMovemment::UP},
+			{KEY_D, EntitieMovemment::RIGTH},
+			{KEY_S, EntitieMovemment::DOWN},
+			{KEY_A, EntitieMovemment::LEFT}
+		};
+
+		for (const KeyDir& input : inputs)
+		{
+			if (IsKeyDown(input.key))
+			{
+				player.player_direction = input.dir;
+				break;
+			}
+			if (!IsKeyDown(input.key))	
+			{
+				player.player_direction = EntitieMovemment::NONE;
+			}
+		}
 	}
 }
--- src/entities/player.h
+++ src/entities/player.h
@@ -4,13 +4,20 @@
 
 namespace tron_seekers
 {
+
+
 	struct Player
 	{
 		Rectangle player_hit_box;
 		Vector2 player_pos;
-		
+
+		float player_speed;
+
+		EntitieMovemment player_direction;
 	};
 
 	void InitPlayer(Player& player);
+	void InputPlayer(Player& player);
+	void UpdatePlayer(Player& player);
 	void DrawPlayer(Player player);
 }
